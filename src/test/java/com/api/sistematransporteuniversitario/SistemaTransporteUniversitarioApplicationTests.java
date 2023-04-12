5ed750f2c8ff609f3ae3cb8e77d4c14253c57c72
viniciusMartinsDev <mpviniciusmp@gmail.com>
2023-04-12 16:34:07 -0300

    feat: configuração inicial do projeto

---
+++ src/test/java/com/api/sistematransporteuniversitario/SistemaTransporteUniversitarioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.sistematransporteuniversitario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaTransporteUniversitarioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
