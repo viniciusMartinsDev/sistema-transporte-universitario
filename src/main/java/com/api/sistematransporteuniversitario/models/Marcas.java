3f95e6fa394be8893f4e3a98ba5e324f7f79c5f5
viniciusMartinsDev <mpviniciusmp@gmail.com>
2023-05-01 18:31:17 -0300

    feat: Implementando o model 'Marcas'

---
+++ src/main/java/com/api/sistematransporteuniversitario/models/Marcas.java
@@ -0,0 +1,19 @@
+package com.api.sistematransporteuniversitario.models;
+
+import jakarta.persistence.*;
+
+import java.io.Serializable;
+import java.util.UUID;
+
+@Entity
+@Table(name = "MARCAS")
+public class Marcas implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+
+    @Column(name = "M_NOME", nullable = false, length = 45)
+    private String marcaNome;
+}
